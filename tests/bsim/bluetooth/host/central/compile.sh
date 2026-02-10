e298ddfe5986c335c5a5ee0b8c17da36c912e1eb
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2026-02-10 13:02:37 +0100

    Added compile script, Kconfig, Kconfig.sysbuild and sysbuild.cmake
    Added compile script, Kconfig, Kconfig.sysbuild and sysbuild.cmake
    for testing on nrf5340bsim/nrf5340/cpuapp.

---
+++ tests/bsim/bluetooth/host/central/compile.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Copyright 2023-2024 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+# Compile all the applications needed by the bsim tests in these subfolders
+
+#set -x #uncomment this line for debugging
+set -ue
+
+: "${ZEPHYR_BASE:?ZEPHYR_BASE must be set to point to the zephyr root\
+ directory}"
+
+source ${ZEPHYR_BASE}/tests/bsim/compile.source
+
+app=tests/bsim/bluetooth/host/central sysbuild=1 compile
+
+wait_for_background_jobs
